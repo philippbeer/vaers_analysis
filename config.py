13dc2933685765881b5ae23ddae07e1c756de989
Philipp Beer <philipp@sciscry.ai>
2021-06-19 12:45:23 +0200

    adding converting nans

--- config.py
+++ config.py
@@ -2,4 +2,15 @@ PATH = "all_data/"
 
 FILENAME_PATTERN = ".*VAERSDATA.csv"
 
+DATE_FORMAT = "%m/%d/%Y"
 
+DATE_COLS = ['RECVDATE',
+             'DATEDIED',
+             'VAX_DATE',
+             'ONSET_DATE',
+             'TODAYS_DATE']
+
+YN_COLS = ['DIED',
+           'HOSPITAL']
+
+YN_REPL_VAL = "N"
--- data_processing.py
+++ data_processing.py
@@ -49,9 +49,57 @@ def compile_df(filepaths: List[str]) -> pd.DataFrame:
         df = df.append(df_tmp)
     return df
 
+def convert_to_datetime(df: pd.DataFrame,
+                        cols: List[str],
+                        date_format: str) -> pd.DataFrame:
+    """
+    converts columns from cols parameter in dataframe to 
+    to datetime objects
+    assumes all columns have same strftime format
+
+    Params:
+    -------
+    df : (dataframe) dataframe with columns to be converted
+    cols : (list of str) list of columns to be converted
+    date_format : (str) strf representation of date format in cols
+    """
+    for col in cols:
+        df[col] = pd.to_datetime(df[col], format=date_format)
+
+    return df
+
+def convert_nans(df: pd.DataFrame,
+                 cols: List[str],
+                 replace_value:str) -> pd.DataFrame:
+    """
+    convert nan-values to defined state
+
+    Params:
+    -------
+    df : (dataframe) dataframe with columns for which to replace nan values
+    cols : (list of str) columns to be converted
+    replace_value : (str) value to replace nans
+    
+    Returns:
+    -------
+    df : (dataframe) with converted columns
+    """
+    for col in cols:
+        df[col].replace(np.nan, replace_value, inplace=True)
+    return df
+
+
 if __name__ == "__main__":
+    # loading data
     fps = setup_filepaths(cnf.PATH,
                           cnf.FILENAME_PATTERN)
 
     df = compile_df(fps)
-    print(df.head())
+
+    # data preprocessing
+    df = convert_to_datetime(df, cnf.DATE_COLS,
+                             cnf.DATE_FORMAT)
+    df = convert_nans(df, cnf.YN_COLS,
+                      cnf.YN_REPL_VAL)
+
+    print(df['DIED'].unique())
